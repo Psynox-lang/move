069cac11e91207f69cf98cf98a400007ee51e0a4
yiungyiung <yashparkhi.super@gmail.com>
2024-02-24 23:16:30 +0530

    Added movement

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -7,30 +7,31 @@ public class movement : MonoBehaviour
     private Vector2 dir = Vector2.right;
     public float speed = 5; 
     public Rigidbody2D rb;
-
+    Quaternion target;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
         Vector2 movement = new Vector2(horizontalInput, verticalInput);
 
         if (movement != Vector2.zero)
         {
             dir = movement.normalized;
             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            target = Quaternion.AngleAxis(angle, Vector3.forward);
+            
+            rb.AddForce(dir * speed,ForceMode2D.Force);
         }
-
-        
-        rb.velocity = dir * speed;
+        //rb.velocity = dir * speed;
+        transform.rotation = Quaternion.Slerp(transform.rotation,target,0.01f);
     }
 }
 
