ce62b1452401e6f6ae29dbca1fe13458010db71e
yiungyiung <yashparkhi.super@gmail.com>
2024-02-24 23:20:54 +0530

    added Velocity Clamp

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     private Vector2 dir = Vector2.right;
-    public float speed = 5; 
+    public float speed = 5;     
+    [SerializeField]
+    float MaxSpeed = 25;
     public Rigidbody2D rb;
     Quaternion target;
     // Start is called before the first frame update
@@ -29,6 +31,10 @@ public class movement : MonoBehaviour
             target = Quaternion.AngleAxis(angle, Vector3.forward);
             
             rb.AddForce(dir * speed,ForceMode2D.Force);
+            if(rb.velocity.magnitude> MaxSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * MaxSpeed;
+            }
         }
         //rb.velocity = dir * speed;
         transform.rotation = Quaternion.Slerp(transform.rotation,target,0.01f);
